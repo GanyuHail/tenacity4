9098665bd7157ae09e0f4ded626c0dbd36ab7bcb
HailGanyu <henryyanguk@gmail.com>
2023-06-06 11:56:24 +0100

    three import statement

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { THREE } from "https://unpkg.com/three@0.120.0"
+import * as THREE from 'three';
 import { EffectComposer } from "https://unpkg.com/three@0.120.0/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "https://unpkg.com/three@0.120.0/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "https://unpkg.com/three@0.120.0/examples/jsm/postprocessing/UnrealBloomPass.js";
